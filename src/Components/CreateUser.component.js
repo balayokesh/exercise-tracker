14b985dd9337d80b43df2e93acf8c90b6e684305
yokesh004 <balayokeshmani@gmail.com>
2021-04-03 11:12:47 +0530

    Added names for routes for testing

--- src/Components/CreateUser.component.js
+++ src/Components/CreateUser.component.js
@@ -1,7 +1,7 @@
 export default function CreateUser () {
     return (
         <>
-            
+            create user
         </>
     );
 }
--- src/Components/EditExercise.component.js
+++ src/Components/EditExercise.component.js
@@ -1,7 +1,7 @@
 export default function EditExercise () {
     return (
         <>
-            
+            edit exercise
         </>
     );
 }
