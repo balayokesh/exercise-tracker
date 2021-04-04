ddb2033ad74c2c795165180bb443725b78c45a88
yokesh004 <balayokeshmani@gmail.com>
2021-04-04 20:16:13 +0530

    completed create user frontend component

--- src/Components/CreateUser.component.js
+++ src/Components/CreateUser.component.js
@@ -1,7 +1,37 @@
+import React, { useState } from 'react';
+
 export default function CreateUser () {
+
+    const [username, setUserName] = useState('');
+
+    const onChangeUserName = (e) => {
+        setUserName(e.target.value);
+    }
+
+    function handleFormSubmit (e) {
+        e.preventDefault();
+        let user = [
+            {username}
+        ];
+        console.log(user);
+        window.location = '/';
+    }
+
     return (
-        <>
-            create user
-        </>
+        <div className='container'>
+            <form onSubmit={handleFormSubmit}>
+                <div className='form-group'>
+                    <label>User Name:</label>
+                    <input 
+                        type='text' 
+                        required
+                        className='form-control'
+                        value={username}
+                        onChange={onChangeUserName}
+                    />
+                </div>
+                <input type='submit' className='btn btn-success' />
+            </form>
+        </div>
     );
 }
